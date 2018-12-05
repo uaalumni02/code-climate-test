61f093e74b03008d909c53921f624a3471cc5ad9
Demeco <dbell1911@gmail.com>
2018-12-04 19:15:50 -0500

    comments added

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 const app = express();
 
+//test for the get request 
 app.get('/test', (req, res) => {
     return res.status(200)
         .send("Test is working");
 });
 
+//test for post request
 app.post('/test', (req, res) => {
     return res.status(200)
         .send("Test is working");
